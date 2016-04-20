c61e71744bc5e04e37ce08c97983bd525d1d2f26
Benoit Hediard <ben@agorapulse.com>
2016-04-20 14:19:58 +0200

    @Log4j annotation added to AmazonKinesisService

--- grails-app/services/grails/plugin/awssdk/kinesis/AmazonKinesisService.groovy
+++ grails-app/services/grails/plugin/awssdk/kinesis/AmazonKinesisService.groovy
@@ -7,6 +7,7 @@ import com.amazonaws.services.kinesis.AmazonKinesisClient
 import com.amazonaws.services.kinesis.model.*
 import grails.core.GrailsApplication
 import grails.plugin.awssdk.AwsClientUtil
+import groovy.util.logging.Log4j
 import org.springframework.beans.factory.InitializingBean
 
 import java.nio.ByteBuffer
@@ -15,6 +16,7 @@ import java.nio.charset.Charset
 import java.nio.charset.CharsetDecoder
 import java.nio.charset.CharsetEncoder
 
+@Log4j
 class AmazonKinesisService implements InitializingBean {
 
     static SERVICE_NAME = ServiceAbbreviations.Kinesis
